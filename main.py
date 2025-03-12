5aa2941e382eeba775acc69edd35d0c39474b69f
Yosri Jemai <91740252+Yosri-Jemai@users.noreply.github.com>
2025-03-12 07:03:33 +0100

    get request

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route("/get-user/id=<user_id>")
+def get_user(user_id):
+    #static data
+    user_data = {
+        "user_id": user_id,
+        "name" : "Yosri Jemai",
+        "email" : "yosrii.jemaiii@gmail.com"
+    }
+
+    # returning dictionary in json format
+    return jsonify(user_data), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
